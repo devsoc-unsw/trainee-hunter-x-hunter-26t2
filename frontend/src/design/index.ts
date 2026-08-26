1d23980decf226d965622a292109166eb27167da
Emily <yanemily2025@gmail.com>
2026-08-26 19:57:49 +1000

    Home page design w tailwind
    will work on it later and popualte w database entries later :3

--- frontend/src/design/index.ts
+++ frontend/src/design/index.ts
@@ -1,3 +1,3 @@
-export { default as Home } from './Home';
+export { default as Home } from '../pages/Home';
 export { default as Profile } from './Profile';
 export { default as Question } from './Question';
